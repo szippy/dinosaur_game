97f4d52a67f292737d9187989ce959a767dcc822
Sarah <41277066+szippy@users.noreply.github.com>
2018-07-22 21:50:01 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.szippy.Dinosaurs;
+
+import java.util.*;
+import java.math.*;
+
+//just some tests for now
+
+public class Main {
+    public static void main(String[] args){
+
+        System.out.print("Project Initialized.");
+        Player p = new Player();
+
+        System.out.print("\nPlayer has " + p.getHealth() + " health");
+        
+    }
+}
