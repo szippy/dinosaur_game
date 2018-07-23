4b665b1ea9a2db9a4e9ebae7cbb20da9c01accf3
Sarah <41277066+szippy@users.noreply.github.com>
2018-07-22 22:01:16 -0600

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,43 @@
+package com.szippy.Dinosaurs;
+
+public class Game {
+    private int time = 0;
+    private int score = 0;
+    private boolean isLost = false;
+
+    // constructor
+    public Game(){
+    }
+    
+    //constructor with time and score
+    public Game(int time, int score){
+        setTime(time);
+        setScore(score);
+    }
+    //setters and getters
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public boolean isLost(){
+        return isLost;
+    }
+    
+    public void setLost(boolean lost) {
+        isLost = lost;
+        //@todo determine if actually lost
+    }
+}
+
