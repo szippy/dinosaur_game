69c5dd05d77d20ea5af87ae140b70d2aecaf5205
Sarah <41277066+szippy@users.noreply.github.com>
2018-07-23 23:43:37 -0600

    Create BigDinosaur.java

---
+++ BigDinosaur.java
@@ -0,0 +1,15 @@
+package com.szippy.Dinosaurs;
+
+public class BigDinosaur extends Dinosaur{
+    private int health = 300;
+    private int speed = 1;
+    private int damage = 30;
+    private String name = "Big Dinosaur";
+
+    //subclassed methods are move, isEating, isStunned
+    public void charge(){
+        //@todo add this special behavior. It moves at speed 5 instead of 1
+
+    }
+
+}
