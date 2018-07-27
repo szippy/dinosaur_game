db09c8e2e73e2f1aaba9abe42588bf393cbe3500
Sarah <41277066+szippy@users.noreply.github.com>
2018-07-26 22:33:48 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,8 +3,6 @@ package com.szippy.Dinosaurs;
 import java.util.*;
 import java.math.*;
 
-//just some tests for now
-
 public class Main {
     public static void main(String[] args){
 
@@ -12,6 +10,9 @@ public class Main {
         Player p = new Player();
 
         System.out.print("\nPlayer has " + p.getHealth() + " health");
-        
+
+        //testing existing window
+        Game g = new Game();
+        g.startGame();
     }
 }
