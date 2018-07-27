c4a1dce1b9ed4c6768d268a74965e69754469373
Sarah <41277066+szippy@users.noreply.github.com>
2018-07-26 22:33:05 -0600

    Create GamePanel.java

---
+++ GamePanel.java
@@ -0,0 +1,32 @@
+package com.szippy.Dinosaurs;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel {
+    JFrame frame;
+
+    public GamePanel(){
+        //default constructor
+        //@todo finish this
+        createWindow();
+    }
+
+    void createWindow(){
+        //draw the new window
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Dinosaur Chase");
+
+        int frameWidth = 600;
+        int frameHeight = 600;
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setBounds((int) screenSize.getWidth() - frameWidth, 0, frameWidth, frameHeight);
+        frame.setVisible(true);
+    }
+
+    void refresh(){
+
+    }
+}
